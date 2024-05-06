19421b1d368147a963b870430a65b05f55fd1ed2
SiwoMikza <169017319+SiwoMikza@users.noreply.github.com>
2024-05-06 12:11:36 +0700

    Create Gilang367.java

---
+++ Gilang367.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Amikom Solo
+ */
+public class Gilang367 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Gilangnurrohmat");
+        
+        
+    }
